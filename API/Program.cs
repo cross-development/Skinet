e1db0ac0c0ef1b2a50c6370c379d9446f94564e4
Vitaliy Derda <derdavitaliy@gmail.com>
2024-09-30 20:12:31 +0300

    feat: added shopping cart api and redis db

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,26 @@
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 using API.Middleware;
 using Infrastructure.Data;
 using Core.Interfaces;
+using Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Redis") ??
+                           throw new InvalidOperationException("Cannot get Redis connection string");
+
+    var configuration = ConfigurationOptions.Parse(connectionString, true);
+
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
 builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));

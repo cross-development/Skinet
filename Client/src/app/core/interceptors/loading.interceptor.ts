5de368b9be56c6dfc133dec1a600605daed40a4d
Vitaliy Derda <derdavitaliy@gmail.com>
2024-11-02 16:30:51 +0200

    feat: preprare client to be published

--- Client/src/app/core/interceptors/loading.interceptor.ts
+++ Client/src/app/core/interceptors/loading.interceptor.ts
@@ -1,12 +1,13 @@
 import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { delay, finalize } from 'rxjs';
+import { delay, finalize, identity } from 'rxjs';
 import { BusyService } from '../services/busy.service';
+import { environment } from '../../../environments/environment';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
 
   busyService.busy();
 
-  return next(req).pipe(delay(500), finalize(busyService.idle));
+  return next(req).pipe(environment.production ? identity : delay(500), finalize(busyService.idle));
 };
